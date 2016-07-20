ad0f6ed49b5a2817650e65556634bf634d45fc35
Dan Sotolongo <dsotolongo@google.com>
2016-07-20 09:12:44 -0700

    update xmonad to lock with gnome

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,23 +1,11 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
+import XMonad.Config.Gnome
 import qualified Data.Map as M
 
-main = do
-  xmproc <- spawnPipe "~/.cabal/bin/xmobar -d ~/.xmonad/.xmobarrc"
-  xmonad $ defaultConfig
-    { manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts $ layoutHook defaultConfig
-    , logHook = dynamicLogWithPP xmobarPP
-        { ppOutput = hPutStrLn xmproc
-        , ppTitle = xmobarColor "green" "" . shorten 50
-        }
-    , keys = customKeys <+> keys defaultConfig
-    }
+main = xmonad $ gnomeConfig {
+    keys = customKeys <+> keys defaultConfig
+  }
 
-customKeys XConfig {XMonad.modMask = modm} = M.fromList
-  [ ((modm .|. shiftMask, xK_z), spawn "slock")
+customKeys XConfig {XMonad.modMask = modm} = M.fromList [
+    ((modm .|. shiftMask, xK_z), spawn "gnome-screensaver-command -l")
   ]
